2d59e6bc8656f7e7b2571e9773a4fce18dbe52b2
dannykhuu2 <dannykhuu2@gmail.com>
2022-11-29 19:36:16 -0500

    what am i doing

--- robotAttack.cpp
+++ robotAttack.cpp
@@ -10,6 +10,8 @@
 #include <vector>
 #include "robotAttack.h"
 
+using namespace std;
+
 GLdouble worldLeft = -12;
 GLdouble worldRight = 12;
 GLdouble worldBottom = -9;
@@ -26,8 +28,37 @@ GLint glutWindowHeight = 600;
 GLint viewportWidth = glutWindowWidth;
 GLint viewportHeight = glutWindowHeight;
 
+// object sizes
+
+double cannonLength = 1.0;
+double cannonWidth = 1.0;
+
 // screen window identifiers
 int window2D, window3D;
+
+// vbo
+
+unsigned int vboId;
+unsigned int iboId;
+
+vector<double> vertices;
+vector<double> normals;
+vector<unsigned int> indices;
+
+bool vboInitialized;
+
+// camera
+
+bool firstMouse = true;
+
+double pitch = 0;
+double yaw = 0;
+
+double positionX = 0.0;
+double positionY = 0.0;
+
+const double PI = 3.14159265;
+const double RADIANS = PI / 180;
 
 int window3DSizeX = 800, window3DSizeY = 600;
 GLdouble aspect = (GLdouble)window3DSizeX / window3DSizeY;
@@ -46,6 +77,11 @@ int main(int argc, char* argv[])
 	glutPositionWindow(900, 100);
 	glutDisplayFunc(display3D);
 	glutReshapeFunc(reshape3D);
+	glutKeyboardFunc(keyboardHandler3D);
+	//glutSetCursor(GLUT_CURSOR_NONE);
+	//glutMouseWheelFunc(mouseScrollWheelHandler3D);
+	//glutMotionFunc(mouseMotionHandler3D);
+	glutSpecialFunc(specialKeyHandler);
 	// Initialize the 3D system
 	init3DSurfaceWindow();
 
@@ -94,12 +130,9 @@ GLdouble fov = 60.0;
 int lastMouseX;
 int lastMouseY;
 
-GLdouble eyeX = 0.0, eyeY = 3.0, eyeZ = 10.0;
+GLdouble eyeX = 0.0, eyeY = 3.0, eyeZ = 30.0;
 GLdouble radius = eyeZ;
 GLdouble zNear = 0.1, zFar = 40.0;
-
-double yaw = 0.0f;
-double pitch = 0.0f;
 
 const double degree = 3.1415 / 180;
 
@@ -108,9 +141,6 @@ unsigned int nId;
 unsigned int vId;
 unsigned int qId;
 
-std::vector<double> vertices;
-std::vector<double> normals;
-std::vector<unsigned int> quads;
 
 void init3DSurfaceWindow()
 {
@@ -170,6 +200,13 @@ void display3D()
 	// Set up the Viewing Transformation (V matrix)	
 	gluLookAt(eyeX, eyeY, eyeZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	drawGround();
+	initVBO();
+
+	glPushMatrix();
+	glRotatef(pitch, 0, 1, 0);
+	glRotatef(-yaw, 1, 0, 0);
+	drawQuads();
+	glPopMatrix();
 
 	// Draw quad mesh
 	glutSwapBuffers();
@@ -184,10 +221,191 @@ void drawGround()
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, groundMat_shininess);
 	glBegin(GL_QUADS);
 	glNormal3f(0, 1, 0);
-	glVertex3f(-22.0f, -4.0f, -22.0f);
-	glVertex3f(-22.0f, -4.0f, 22.0f);
-	glVertex3f(22.0f, -4.0f, 22.0f);
-	glVertex3f(22.0f, -4.0f, -22.0f);
+	glVertex3f(-50.0f, -4.0f, -50.0f);
+	glVertex3f(-50.0f, -4.0f, 50.0f);
+	glVertex3f(50.0f, -4.0f, 50.0f);
+	glVertex3f(50.0f, -4.0f, -50.0f);
 	glEnd();
 	glPopMatrix();
+}
+
+void initVBO()
+{
+	if (!vboInitialized) {
+		glGenBuffers(1, &vboId);
+		glGenBuffers(1, &iboId);
+
+		string line;
+		ifstream MyFile("mesh.obj");
+		while (getline(MyFile, line))
+		{
+			double x, y, z = 0.0;
+			int f1, f2, f3, f4 = 0;
+			istringstream iss(line);
+			string context;
+			iss >> context;
+
+			if (context == "v") {
+				iss >> x >> y >> z;
+				vertices.push_back(x);
+				vertices.push_back(y);
+				vertices.push_back(z);
+			}
+			else if (context == "vn") {
+				iss >> x >> y >> z;
+				normals.push_back(x);
+				normals.push_back(y);
+				normals.push_back(z);
+			}
+			else if (context == "f") {
+				iss >> f1 >> f2 >> f3 >> f4;
+				indices.push_back(f1);
+				indices.push_back(f2);
+				indices.push_back(f3);
+				indices.push_back(f4);
+			}
+		}
+		MyFile.close();
+
+		vboInitialized = true;
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() + normals.size() * sizeof(double), 0, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(double), &vertices[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, vertices.size() * sizeof(double), normals.size() * sizeof(double), &normals[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void drawQuads()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
+
+	glVertexPointer(3, GL_DOUBLE, 0, (void*)0);
+	glNormalPointer(GL_DOUBLE, 0, (void*)0);
+
+	glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_INT, (void*)0);
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void mouseMotionHandler3D(int x, int y)
+{
+	if (firstMouse) {
+		lastMouseX = x;
+		lastMouseY = y;
+		firstMouse = false;
+	}
+
+	double hypoteneuse = sqrt(pow(radius, 2) + pow(eyeY, 2));
+	int dx = x - lastMouseX;
+	int dy = y - lastMouseY;
+
+	lastMouseX = x;
+	lastMouseY = y;	
+
+	//if (x < 50 || x > window3DSizeX - 50) {
+	//	lastMouseX = window3DSizeX / 2;
+	//	lastMouseY = window3DSizeY / 2;
+	//	glutWarpPointer(window3DSizeX / 2, window3DSizeY / 2);
+	//}
+	//else if (y < 50 || y > window3DSizeY - 50) {
+	//	lastMouseX = window3DSizeX / 2;
+	//	lastMouseY = window3DSizeY / 2;
+	//	glutWarpPointer(window3DSizeX / 2, window3DSizeY / 2);
+	//}
+
+	float sensitivity = 0.1f;
+	dx *= sensitivity;
+	dy *= sensitivity;
+
+	yaw += dx;
+	pitch += dy;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	eyeX = cos(RADIANS * yaw) * cos(RADIANS * pitch) * hypoteneuse;
+	eyeY = sin(RADIANS * pitch) * hypoteneuse;
+	eyeZ = sin(RADIANS * yaw) * cos(RADIANS * pitch) * hypoteneuse;
+
+	glutPostRedisplay();
+}
+
+void mouseScrollWheelHandler3D(int button, int dir, int xMouse, int yMouse)
+{
+	// Fill in this code for zooming in and out
+	fov -= (GLdouble)(dir);
+	if (fov < 1.0)
+		fov = 1.0;
+	if (fov > 175.0)
+		fov = 175.0;
+	glutPostRedisplay();
+}
+
+void keyboardHandler3D(unsigned char key, int x, int y)
+{
+
+	switch (key)
+	{
+	case 'q':
+	case 'Q':
+	case 27:
+		// Esc, q, or Q key = Quit 
+		exit(0);
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void specialKeyHandler(int key, int x, int y)
+{
+	//double hypoteneuse = sqrt(pow(radius, 2) + pow(eyeY, 2));
+
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		yaw -= 5.0;
+		break;
+	case GLUT_KEY_RIGHT:
+		yaw += 5.0;
+		break;
+	case GLUT_KEY_UP:
+		pitch -= 2.0;
+		break;
+	case GLUT_KEY_DOWN:
+		pitch += 2.0;
+		break;
+	}
+
+	if (yaw > 60.0)
+		yaw = 60.0;
+	if (yaw < 0.0)
+		yaw = 0;
+
+	if (pitch > 20.0)
+		pitch = 20.0;
+	if (pitch < 0)
+		pitch = 0;
+
+	eyeX = cos(RADIANS * yaw) * cos(RADIANS * pitch) * radius;
+	eyeY = sin(RADIANS * pitch) * radius;
+	eyeZ = sin(RADIANS * yaw) * cos(RADIANS * pitch) * radius;
+	glutPostRedisplay();
 }
